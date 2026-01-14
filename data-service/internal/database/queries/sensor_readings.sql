18f6a9ad0cfcad9d3e797843e3703d8dcfa67beb
iJoyRide <sumfeis3@gmail.com>
2026-01-14 08:22:14 +0000

    get endpoints for timescale DB

--- data-service/internal/database/queries/sensor_readings.sql
+++ data-service/internal/database/queries/sensor_readings.sql
@@ -1,3 +1,8 @@
 -- name: InsertSensorReading :exec
 INSERT INTO sensor_readings (timestamp, sensor, sensor_id, value)
 VALUES ($1, $2, $3, $4);
+
+SELECT time_bucket('5 min', time) AS bucket 
+FROM sensor_readings
+WHERE timestamp BETWEEN $1 AND $2
+ORDER BY timestamp ASC
